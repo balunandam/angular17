eb09355dd47c4881fa3178569f655f08a909fc29
Baladithya Nandam <baladithya.nandam@hallmarkhcs.com>
2024-02-01 12:11:17 +0530

    angular 17 practise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+interface userData {
+id: string,
+name: string,
+class: string
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UpperCasePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'test';
+  user = signal<userData[]>([
+    {id: '1', name: 'balu', class: 'developer'},
+    {id: '2', name: 'kiran', class: 'qa'},
+    {id: '3', name: 'yashu', class: 'admin'},
+  ])
 }
